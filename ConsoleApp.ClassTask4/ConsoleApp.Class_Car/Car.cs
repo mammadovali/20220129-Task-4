a1c766e8d89d617c9b72201873aea1f94ebf3398
mammadovali <aliam@code.edu.az>
2022-01-30 20:03:26 +0400

    2 cars selected from turbo.az

---
+++ ConsoleApp.ClassTask4/ConsoleApp.Class_Car/Car.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp.Class_Car
+{
+    internal class Car
+    {
+        public string city;
+        public string name;
+        public string model;
+        public int year;
+        public string type;
+        public string color;
+        public double engine;
+        public double horsePower;
+        public string fuelType;
+        public int distance;
+        public string gearBox;
+        public string transmission;
+        public string New;
+        public int price; 
+
+        public string GetInfo()
+        {
+            return $"Şəhər: {city} \n" +
+             $"Marka: {name} \n" +
+             $"Model: {model} \n" +
+             $"Buraxılış ili: {year} \n" +
+             $"Ban növü: {type} \n" +
+             $"Rəng: {color} \n" +
+             $"Mühərrik: {engine} \n" +
+             $"Mühərrikin gücü: {horsePower} a.g. \n" +
+             $"Yanacaq növü: {fuelType} \n" +
+             $"Yürüş: {distance} km \n" +
+             $"Sürətlər qutusu: {gearBox} \n" +
+             $"Ötürücü: {transmission} \n" +
+             $"Yeni: {New} \n" +
+             $"Qiymət: {price} $";
+        }
+
+    }
+}
---
+++ ConsoleApp.ClassTask4/ConsoleApp.Class_Car/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Text;
+
+namespace ConsoleApp.Class_Car
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.InputEncoding = Encoding.Unicode;
+            Console.OutputEncoding = Encoding.Unicode;
+
+            Car bmw = new Car();
+
+            bmw.city = "Bakı";
+            bmw.name = "BMW";
+            bmw.model = "M5";
+            bmw.year = 2014;
+            bmw.type = "Sedan";
+            bmw.color = "Boz";
+            bmw.engine = 4.4;
+            bmw.horsePower = 575;
+            bmw.fuelType = "Benzin";
+            bmw.distance = 96000;
+            bmw.gearBox = "Avtomat";
+            bmw.transmission = "Arxa";
+            bmw.New = "Xeyr";
+            bmw.price = 55000;
+
+            Car landRover = new Car();
+            landRover.city = "Bakı";
+            landRover.name = "Land Rover";
+            landRover.model = "RR Sport";
+            landRover.year = 2019;
+            landRover.type = "Offroader / SUV";
+            landRover.color = "Qırmızı";
+            landRover.engine = 3.0;
+            landRover.horsePower = 360;
+            landRover.fuelType = "Benzin";
+            landRover.distance = 6500;
+            landRover.gearBox = "Avtomat";
+            landRover.transmission = "Tam";
+            landRover.New = "Xeyr";
+            landRover.price = 105000;
+
+            Car[] cars = new Car[2];
+
+            cars[0] = bmw;
+            cars[1] = landRover;
+
+            foreach (Car item in cars)
+            {
+                Console.WriteLine(item.GetInfo());
+                Console.WriteLine("--------");
+                
+            }
+
+
+
+
+
+
+
+        }
+    }
+}
